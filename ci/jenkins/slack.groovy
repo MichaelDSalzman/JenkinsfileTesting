6696d4b7fc3114c5792a2f55f686e668c440da1d
Michael Salzman <michael.salzman@projekt202.com>
2021-10-07 11:41:17 -0500

    Playing around with slack

--- ci/jenkins/slack.groovy
+++ ci/jenkins/slack.groovy
@@ -1,9 +1,11 @@
-def sendSuccessMessage() {
+def sendSuccessMessage(String message) {
   echo "SUCCESS - Called from slack"
+  slackSend(color:"good", message: message)
 }
 
-def sendFailureMessage() {
+def sendFailureMessage(String message) {
   echo "FAILURE - Called from slack"
+  slackSend(color:"danger", message: message)
 }
 
 return this
