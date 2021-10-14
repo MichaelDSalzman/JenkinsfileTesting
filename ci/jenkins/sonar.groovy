2e921b71b8b0624b30b8664ce01ce90ac9d018ff
Michael Salzman <michael.salzman@projekt202.com>
2021-10-14 15:03:43 -0500

    Really in the weeds now

--- ci/jenkins/sonar.groovy
+++ ci/jenkins/sonar.groovy
@@ -3,7 +3,7 @@ def scan(def onFailure) {
       script {
           // def statusCode = sh(script: "SONAR_URL=${SONAR_HOST_URL} SONAR_LOGIN=${SONAR_AUTH_TOKEN} npm run sonarVerify", returnStatus:true )
         //   def statusCode = sh(script: "npm run sonar", returnStatus:true )
-          def statusCode = sh(script: "npm run sonarMike", returnStatus:true )
+          def statusCode = sh(script: "SONAR_PROJECT_KEY=AppFramework SONAR_URL=${SONAR_HOST_URL} SONAR_LOGIN=${SONAR_AUTH_TOKEN} npm run sonarMike", returnStatus:true )
           if(statusCode != 0) {
               onFailure("SonarQube failed. See http://${SONAR_HOST_URL} for more details")
           }
--- tool/sonar.js
+++ tool/sonar.js
@@ -1,7 +1,28 @@
 const verifier = require("sonarqube-verify/lib/sonarqube-verify");
 
+const projectKey =
+  process.env.SONAR_PROJECT_KEY || pkg.name.replace("@", "").replace("/", ":");
+
+// sonar parameters
+const sonarUrl = process.env.SONAR_URL || "http://localhost:9000";
+const sonarLogin = process.env.SONAR_LOGIN || "";
+const sonarPassword = process.env.SONAR_PASSWORD || "";
+const sonarSkip = process.env.SONAR_SKIP || false;
+const gateSkip = process.env.SONAR_GATE_SKIP || false;
+
+const params = {
+  "sonar.projectKey": projectKey,
+  "sonar.projectVersion": pkg.version,
+  "sonar.projectName": pkg.name,
+  "sonar.host.url": sonarUrl,
+  "sonar.login": sonarLogin,
+  "sonar.skip": sonarSkip,
+  "sonar.gate.skip": gateSkip,
+  "sonar.password": sonarPassword,
+};
+
 verifier
-  .verify({})
+  .verify(params)
   .then((result) => {
     console.log("Verification ended in success");
     console.log(result);
