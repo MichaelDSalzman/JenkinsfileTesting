3e10356b2ca2c31b2163d287890fd89d14e9aa51
Michael Salzman <michael.salzman@projekt202.com>
2021-10-14 10:55:17 -0500

    Sonar take 1

---
+++ tool/sonar.js
@@ -0,0 +1,15 @@
+const scanner = require("sonarqube-scanner");
+
+scanner(
+  {
+    serverUrl: "http://34.217.144.28",
+    token: "3d568c20b05582b00e58ab2b690a30694227ba56",
+    options: {
+      "sonar.projectName": "LocalJenkinsfile",
+      "sonar.projectDescription": 'Description for "My App" project...',
+      //"sonar.sources": "dist",
+      //"sonar.tests": "specs",
+    },
+  },
+  () => process.exit()
+);
