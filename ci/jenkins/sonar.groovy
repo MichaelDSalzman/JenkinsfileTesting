0ab8ddb2e352d350bdb67244a8a7c387361a9cf7
Michael Salzman <michael.salzman@projekt202.com>
2021-10-14 13:46:17 -0500

    Send message before throwing exception

--- ci/jenkins/sonar.groovy
+++ ci/jenkins/sonar.groovy
@@ -4,10 +4,10 @@ def scan() {
           def statusCode = sh(script: "SONAR_URL=${SONAR_HOST_URL} SONAR_LOGIN=${SONAR_AUTH_TOKEN} npm run sonarVerify", returnStatus:true)
 
           if(statusCode != 0) {
-              throw new Exception("SonarQube scan failure")
-
               def slackLib = load "ci/jenkins/slack.groovy"
               slackLib.sendFailureMessage("SonarQube failed")
+
+              throw new Exception("SonarQube scan failure")
           }
       }
   } 
