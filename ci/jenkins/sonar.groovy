0a96b5badd6728abb20a7a8262e4c81dc61bd80a
Michael Salzman <michael.salzman@projekt202.com>
2021-10-14 14:06:29 -0500

    tee output

--- ci/jenkins/sonar.groovy
+++ ci/jenkins/sonar.groovy
@@ -1,9 +1,9 @@
 def scan(def onFailure) {
   withSonarQubeEnv("SonarEC2") {
       script {
-          def statusCode = sh(script: "SONAR_URL=${SONAR_HOST_URL} SONAR_LOGIN=${SONAR_AUTH_TOKEN} npm run sonarVerify", returnStatus:true)
-
+          def statusCode = sh(script: "SONAR_URL=${SONAR_HOST_URL} SONAR_LOGIN=${SONAR_AUTH_TOKEN} npm run sonarVerify", returnStatus:true | tee sonar.out)
           if(statusCode != 0) {
+
               onFailure("SonarQube failed. See http://${SONAR_HOST_URL}/dashboard?id=TODO for more details")
           }
       }
