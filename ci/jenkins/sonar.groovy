2fbaa24794f5dd73ace28e95628b8f4c9efb4f64
Michael Salzman <michael.salzman@projekt202.com>
2021-10-14 13:50:09 -0500

    Adding failure callback

--- ci/jenkins/sonar.groovy
+++ ci/jenkins/sonar.groovy
@@ -1,13 +1,10 @@
-def scan() {
+def scan(def failureCallback) {
   withSonarQubeEnv("SonarEC2") {
       script {
           def statusCode = sh(script: "SONAR_URL=${SONAR_HOST_URL} SONAR_LOGIN=${SONAR_AUTH_TOKEN} npm run sonarVerify", returnStatus:true)
 
           if(statusCode != 0) {
-              def slackLib = load "ci/jenkins/slack.groovy"
-              slackLib.sendFailureMessage("SonarQube failed")
-
-              throw new Exception("SonarQube scan failure")
+              failureCallback("SonarQube failed")
           }
       }
   } 
