3b3b2e3ef98c6d79ad9fb9145fcf47a00f8fb01b
Michael Salzman <michael.salzman@projekt202.com>
2021-10-15 11:03:57 -0500

    Adding quality gate

--- ci/jenkins/sonar.groovy
+++ ci/jenkins/sonar.groovy
@@ -19,7 +19,7 @@ def scan(def onFailure) {
          // Just in case something goes wrong, pipeline will be killed after a timeout
             def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
             if (qg.status != 'OK') {
-                error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                onFailure("Pipeline aborted due to sonar quality gate failure: ${qg.status}")
             }
         }
       }
