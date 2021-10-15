de1ddb12382e546a4aac5eb0f4c281b4749d3d56
Michael Salzman <michael.salzman@projekt202.com>
2021-10-15 09:58:35 -0500

    Adding quality gate

--- ci/jenkins/sonar.groovy
+++ ci/jenkins/sonar.groovy
@@ -1,6 +1,7 @@
 def scan(def onFailure) {
   withSonarQubeEnv("SonarEC2") {
       script {
+          timeout(time: 1, unit: 'HOURS') {
           // def statusCode = sh(script: "SONAR_URL=${SONAR_HOST_URL} SONAR_LOGIN=${SONAR_AUTH_TOKEN} npm run sonarVerify", returnStatus:true )
           sh(script: "npm run sonar" )
         //   sh(script: "SONAR_PROJECT_KEY=AppFramework SONAR_URL=${SONAR_HOST_URL} SONAR_LOGIN=${SONAR_AUTH_TOKEN} npm run sonarMike" )
@@ -8,7 +9,7 @@ def scan(def onFailure) {
         //       onFailure("SonarQube failed. See http://${SONAR_HOST_URL} for more details")
         //   }
 
-        timeout(time: 1, unit: 'HOURS') { // Just in case something goes wrong, pipeline will be killed after a timeout
+         // Just in case something goes wrong, pipeline will be killed after a timeout
             def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
             if (qg.status != 'OK') {
                 error "Pipeline aborted due to quality gate failure: ${qg.status}"
