634662efcdab6e42517e0f439612b256d95b2060
Michael Salzman <michael.salzman@projekt202.com>
2021-10-15 12:18:56 -0500

    Move stuff around

--- ci/jenkins/sonar.groovy
+++ ci/jenkins/sonar.groovy
@@ -3,32 +3,30 @@ def scan(def onFailure) {
       script {
         timeout(time: 1, unit: 'HOURS') {
             sh(script: "npm run sonar" )
-            
+
+echo "MIKE1"
+            sh "ls -ltr ./.scannerwork"
         }
       }
+  } 
 
+echo "MIKE2"
+            sh "ls -ltr ./.scannerwork"
+    withSonarQubeEnv("SonarEC2") {
+echo "MIKE3"
+            sh "ls -ltr ./.scannerwork"
       script {
-          def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
+          timeout(time: 1, unit: 'HOURS') {
+
+         // Just in case something goes wrong, pipeline will be killed after a timeout
+            def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
 
             if (qg.status != 'OK') {
                 onFailure("Pipeline aborted due to sonar quality gate failure: ${qg.status}")
             }
+        }
       }
   } 
-
-//     withSonarQubeEnv("SonarEC2") {
-//       script {
-//           timeout(time: 1, unit: 'HOURS') {
-
-//          // Just in case something goes wrong, pipeline will be killed after a timeout
-//             def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
-
-//             if (qg.status != 'OK') {
-//                 onFailure("Pipeline aborted due to sonar quality gate failure: ${qg.status}")
-//             }
-//         }
-//       }
-//   } 
 }
 
 return this
