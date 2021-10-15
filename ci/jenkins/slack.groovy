dec8688032ed634e4776ceec09effe708a424149
Michael Salzman <michael.salzman@projekt202.com>
2021-10-15 12:14:20 -0500

    Move stuff around

--- ci/jenkins/slack.groovy
+++ ci/jenkins/slack.groovy
@@ -1,11 +1,11 @@
 def sendSuccessMessage(String message) {
   echo "SUCCESS - Called from slack"
-  slackSend(color:"good", message: "<$BUILD_URL|Build> $message")
+  slackSend(color:"good", message: "<$BUILD_URL|Build> - $message")
 }
 
 def sendFailureMessage(String message) {
   echo "FAILURE - Called from slack"
-  slackSend(color:"danger", message: "<$BUILD_URL|Build> $message")
+  slackSend(color:"danger", message: "<$BUILD_URL|Build> - $message")
 }
 
 return this
--- ci/jenkins/sonar.groovy
+++ ci/jenkins/sonar.groovy
@@ -1,29 +1,30 @@
 def scan(def onFailure) {
   withSonarQubeEnv("SonarEC2") {
       script {
-          timeout(time: 1, unit: 'HOURS') {
-          // def statusCode = sh(script: "SONAR_URL=${SONAR_HOST_URL} SONAR_LOGIN=${SONAR_AUTH_TOKEN} npm run sonarVerify", returnStatus:true )
-          sh(script: "npm run sonar" )
-        //   sh(script: "SONAR_PROJECT_KEY=AppFramework SONAR_URL=${SONAR_HOST_URL} SONAR_LOGIN=${SONAR_AUTH_TOKEN} npm run sonarMike" )
-        //   if(statusCode != 0) {
-        //       onFailure("SonarQube failed. See http://${SONAR_HOST_URL} for more details")
-        //   }
-        }
-      }
-  } 
-
-    withSonarQubeEnv("SonarEC2") {
-      script {
-          timeout(time: 1, unit: 'HOURS') {
-
-         // Just in case something goes wrong, pipeline will be killed after a timeout
+        timeout(time: 1, unit: 'HOURS') {
+            sh(script: "npm run sonar" )
             def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
+
             if (qg.status != 'OK') {
                 onFailure("Pipeline aborted due to sonar quality gate failure: ${qg.status}")
             }
         }
       }
   } 
+
+//     withSonarQubeEnv("SonarEC2") {
+//       script {
+//           timeout(time: 1, unit: 'HOURS') {
+
+//          // Just in case something goes wrong, pipeline will be killed after a timeout
+//             def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
+
+//             if (qg.status != 'OK') {
+//                 onFailure("Pipeline aborted due to sonar quality gate failure: ${qg.status}")
+//             }
+//         }
+//       }
+//   } 
 }
 
 return this
