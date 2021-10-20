cdf558a7e45476cedd833820f1307165c7939ad2
Michael Salzman <michael.salzman@projekt202.com>
2021-10-20 11:27:42 -0500

    Testing attachments

--- ci/jenkins/slack.groovy
+++ ci/jenkins/slack.groovy
@@ -18,7 +18,6 @@ def sendFailureMessage(String message) {
 
   def attachments = [
 [
-        
 	        mrkdwn_in: ["text"],
             color: "#36a64f",
             pretext: "Optional pre-text that appears above the attachment block",
@@ -29,27 +28,26 @@ def sendFailureMessage(String message) {
             title_link: "https://api.slack.com/",
             text: "Optional `text` that appears within the attachment",
             fields: [
-                {
+                [
                     title: "A field's title",
                     value: "This field's value",
                     short: false
-                },
-                {
+                ],
+                [
                     title: "A short field's title",
                     value: "A short field's value",
                     short: true
-                },
-                {
+                ],
+                [
                     title: "A second short field's title",
                     value: "A second short field's value",
                     short: true
-                }
+                ]
             ],
             thumb_url: "http://placekitten.com/g/200/200",
             footer: "footer",
             footer_icon: "https://platform.slack-edge.com/img/default_application_icon.png",
             ts: 123456789
-        
     ]
 ]
 
