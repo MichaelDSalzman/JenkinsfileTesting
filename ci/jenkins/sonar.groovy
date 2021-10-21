60af80faa0fb60e8af728d82bfaadd7fab5e5654
Michael Salzman <michael.salzman@projekt202.com>
2021-10-21 09:31:30 -0500

    Ping sonar API

--- ci/jenkins/sonar.groovy
+++ ci/jenkins/sonar.groovy
@@ -1,21 +1,34 @@
 def scan(def onFailure) {
   withSonarQubeEnv {
-    sh(script: "npm run sonar" )
-  } 
+      sh 'env'
+    sh(script: 'npm run sonar' )
+  }
 
-    withSonarQubeEnv {
+  withSonarQubeEnv {
+      sh 'env'
       script {
           timeout(time: 1, unit: 'HOURS') {
-
-         // Just in case something goes wrong, pipeline will be killed after a timeout
+            // Just in case something goes wrong, pipeline will be killed after a timeout
             def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
 
+        def SONAR_TASK_RESP = sh(returnStdout: true, script: ". .scannerwork/report-task.txt && curl -s -u ${SONAR_AUTH_TOKEN}: \$ceTaskUrl")
+        def SONAR_ANALYSIS_ID = new groovy.json.JsonSlurper().parseText(SONAR_TASK_RESP).task.analysisId
+
+        def QUALITY_GATE_URL = "${SONAR_HOST_URL}/api/qualitygates/project_status?analysisId=$SONAR_ANALYSIS_ID"
+        def QUALITY_RESP = sh(returnStdout: true, script: "curl -s -u ${SONAR_AUTH_TOKEN}: $QUALITY_GATE_URL")
+
+        def QUALITY_MAP = new groovy.json.JsonSlurper().parseText(QUALITY_RESP)
+
+        List<String> failureDetails = QUALITY_MAP['projectStatus']['conditions'].findAll { it['status'] == 'ERROR' }.collect {
+            "${it['metricKey']} is in an error state. Threshold must be ${it.comparator} ${it.errorThreshold}. Actual value is ${it.actualValue}."
+          }
+
             if (qg.status != 'OK') {
-                onFailure("Pipeline aborted due to sonar quality gate failure: ${qg.status}")
+          onFailure("Pipeline aborted due to sonar quality gate failure: ${qg.status} - ${failureDetails}")
             }
-        }
       }
-  } 
+  }
+}
 }
 
 return this
