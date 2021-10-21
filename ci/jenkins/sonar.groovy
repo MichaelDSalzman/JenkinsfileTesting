d808cc990f008095f458b861b1a364c032be1309
Michael Salzman <michael.salzman@projekt202.com>
2021-10-21 12:41:57 -0500

    Use pipeline utils

--- ci/jenkins/sonar.groovy
+++ ci/jenkins/sonar.groovy
@@ -13,21 +13,19 @@ def scan(def onFailure) {
 
         if (qg.status != 'OK') {
           // Retrieve sonar task url and curl to get analysis id
-          def SONAR_TASK_RESP = sh(returnStdout: true, script: ". .scannerwork/report-task.txt && curl -s -u ${SONAR_AUTH_TOKEN}: \$ceTaskUrl")
-          def SONAR_ANALYSIS_ID = new groovy.json.JsonSlurperClassic().parseText(SONAR_TASK_RESP).task.analysisId
-
-          // Parse sonar properties file to get dashboard URL
-          def SONAR_DASHBOARD_URL = sh(returnStdout: true, script: ". .scannerwork/report-task.txt && echo \$dashboardUrl")
+          def SONAR_TASK_PROPERTIES = readProperties file: ".scannerwork/report-task.txt"
+          def SONAR_TASK_RESP = sh(returnStdout: true, script: "curl -s -u ${SONAR_AUTH_TOKEN}: ${SONAR_TASK_PROPERTIES.ceTaskUrl} > .scannerwork/sonar-task.json")
+          def SONAR_TASK_MAP = readJSON file: '.scannerwork/sonar-task.json'
 
           // Retrieve quality report
-          def QUALITY_GATE_URL = "${SONAR_HOST_URL}/api/qualitygates/project_status?analysisId=$SONAR_ANALYSIS_ID"
-          def QUALITY_RESP = sh(returnStdout: true, script: "curl -s -u ${SONAR_AUTH_TOKEN}: $QUALITY_GATE_URL")
-          def QUALITY_MAP = new groovy.json.JsonSlurperClassic().parseText(QUALITY_RESP)
+          def QUALITY_GATE_URL = "${SONAR_HOST_URL}/api/qualitygates/project_status?analysisId=${SONAR_TASK_MAP.task.analysisId}"
+          def QUALITY_RESP = sh(returnStdout: true, script: "curl -s -u ${SONAR_AUTH_TOKEN}: $QUALITY_GATE_URL > .scannerwork/sonar-quality.json")
+          def QUALITY_MAP = readJSON file: '.scannerwork/sonar-quality.json'
 
           // Filter out conditions that failed
           List failures = QUALITY_MAP['projectStatus']['conditions'].findAll { it['status'] == 'ERROR' }//.collect {
 
-          onFailure("Sonar quality failures:", failures, SONAR_DASHBOARD_URL)
+          onFailure("Sonar quality failures:", failures, SONAR_TASK_PROPERTIES.dashboardUrl)
         }
       }
     }
