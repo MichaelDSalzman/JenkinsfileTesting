073c61e3c477a466e5b693bb5364793ae8693343
Michael Salzman <michael.salzman@projekt202.com>
2021-10-21 15:34:54 -0500

    Introduce env

--- ci/jenkins/envVarLoader.groovy
+++ ci/jenkins/envVarLoader.groovy
@@ -2,8 +2,8 @@ def loadEnvProps(String env) {
   def props = readProperties file: "ci/jenkins/${env}.env"
   keys = props.keySet()
   for (key in keys) {
-    value = props[key]
-    env[key] = value
+    value = props["${key}"]
+    env."${key} = "${value}"
   }
 }
 
