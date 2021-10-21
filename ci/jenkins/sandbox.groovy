d484f6b6b9e5dda4907b2a4b6cd08fcd4b1cd4e3
Michael Salzman <michael.salzman@projekt202.com>
2021-10-21 16:16:08 -0500

    Going crazy

--- ci/jenkins/sandbox.groovy
+++ ci/jenkins/sandbox.groovy
@@ -3,7 +3,7 @@ env.sonar_failure_slack_title="Sandbox build failed on branch $GIT_BRANCH"
 env.sonar_failure_slack_title_link="$BUILD_URL"
 env.sonar_timeoutInMinutes=10
 
-env.sonar_failure_fn={String message, List failures, String sonarDashboardUrl ->
+env.sonar_failure_fn = {String message, List failures, String sonarDashboardUrl ->
                         slackLib.sendDetailedMessage(
                             [
                                 color: env.sonar_failure_slack_color, 
