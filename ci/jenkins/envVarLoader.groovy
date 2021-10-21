0a880ebcd5f9fff72a0dc992ffa33f4197528ec1
Michael Salzman <michael.salzman@projekt202.com>
2021-10-21 15:40:28 -0500

    print out env

--- ci/jenkins/envVarLoader.groovy
+++ ci/jenkins/envVarLoader.groovy
@@ -2,7 +2,9 @@ def loadEnvProps(String env) {
   def props = readProperties file: "ci/jenkins/${env}.env"
   keys = props.keySet()
   for (key in keys) {
+    echo "GOT KEY $key"
     value = props["${key}"]
+    echo "GOT VALUE $value"
     env."${key}" = "${value}"
   }
 }
