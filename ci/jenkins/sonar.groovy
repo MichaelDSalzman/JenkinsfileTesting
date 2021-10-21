d414373015263fcf2eb3f78157834b6fbf5145b8
Michael Salzman <michael.salzman@projekt202.com>
2021-10-21 10:29:45 -0500

    Send sonar link

--- ci/jenkins/sonar.groovy
+++ ci/jenkins/sonar.groovy
@@ -15,6 +15,8 @@ def scan(def onFailure) {
           def SONAR_TASK_RESP = sh(returnStdout: true, script: ". .scannerwork/report-task.txt && curl -s -u ${SONAR_AUTH_TOKEN}: \$ceTaskUrl")
           def SONAR_ANALYSIS_ID = new groovy.json.JsonSlurper().parseText(SONAR_TASK_RESP).task.analysisId
 
+          def SONAR_DASHBOARD_URL = sh(returnStdout: true, script: ". .scannerwork/report-task.txt && echo \$dashboardUrl")
+
           def QUALITY_GATE_URL = "${SONAR_HOST_URL}/api/qualitygates/project_status?analysisId=$SONAR_ANALYSIS_ID"
           def QUALITY_RESP = sh(returnStdout: true, script: "curl -s -u ${SONAR_AUTH_TOKEN}: $QUALITY_GATE_URL")
 
@@ -23,7 +25,7 @@ def scan(def onFailure) {
           List failures = QUALITY_MAP['projectStatus']['conditions'].findAll { it['status'] == 'ERROR' }//.collect {
             //"${it['metricKey']} is in an error state. Threshold must be ${it.comparator} ${it.errorThreshold}. Actual value is ${it.actualValue}."
           //}
-          onFailure("Pipeline aborted due to sonar quality gate failure", failures)
+          onFailure("Pipeline aborted due to <$SONAR_DASHBOARD_URL|sonar quality gate> failures:", failures)
         }
       }
     }
