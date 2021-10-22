c012241917d32260aa90a0d1b955b0e11ca7fa84
Michael Salzman <michael.salzman@projekt202.com>
2021-10-22 10:11:34 -0500

    Trying stuff again

--- ci/jenkins/envVarLoader.groovy
+++ ci/jenkins/envVarLoader.groovy
@@ -1,16 +1,12 @@
 def loadEnvProps(String env) {
-                      def key = "test"
-                    def value = "FOOBAR"
-                    env."${key}" = "$value"
-                    echo env."test"
-  // def props = readProperties file: "ci/jenkins/${env}.env"
-  // keys = props.keySet()
-  // for (key in keys) {
-  //   echo "GOT KEY $key"
-  //   value = props["${key}"]
-  //   echo "GOT VALUE $value"
-  //   env."${key}" = "$value"
-  // }
+  def props = readProperties file: "ci/jenkins/${env}.env"
+  keys = props.keySet()
+  for (key in keys) {
+    echo "GOT KEY $key"
+    value = props["${key}"]
+    echo "GOT VALUE $value"
+    env."${key}" = "$value"
+  }
 }
 
 return this
