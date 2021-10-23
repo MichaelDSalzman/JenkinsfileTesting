ccdddb483bed63f5757ddeba58545b4c66996222
Michael Salzman <michael.salzman@projekt202.com>
2021-10-22 20:21:42 -0500

    tweak

---
+++ ci/helpers/message_builders.groovy
@@ -0,0 +1,26 @@
+def generateSonarFailureDetailedSlackMessage(List failedConditions, String sonarDashboardUrl) {
+  def fields = failedConditions.collect {
+    def comparator = it.comparator
+    switch (comparator) {
+        case "GT":
+            comparator = env."sonar_failure_slack_comparator_gt"
+            break
+        case "LT":
+            comparator = env."sonar_failure_slack_comparator_lt"
+            break
+    }
+    return [
+      title: "${env."sonar_failure_slack_failure_type_header"}: ${it.metricKey.replaceAll('_', ' ')}", 
+      value: "${env."sonar_failure_slack_failure_type_body_threshold"} ${comparator} ${it.errorThreshold}\n${env."sonar_failure_slack_failure_type_body_actual"}: ${it.actualValue}", 
+      short: false
+    ]
+  }
+  return [
+      color: "danger",
+      title: env."sonar_failure_slack_title",
+      titleLink: "$BUILD_URL",
+      text: env."sonar_failure_slack_subtitle",
+      fields: [[title: env."sonar_failure_slack_report_url_header", value: sonarDashboardUrl]] + failures
+  ]
+}
+return this
