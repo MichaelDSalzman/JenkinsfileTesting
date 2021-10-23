d7eea0922748927eb0eb5072b8d2e7d152758e7c
Michael Salzman <michael.salzman@projekt202.com>
2021-10-23 18:17:47 -0500

    Interpolation damnit

--- ci/helpers/envLoader.groovy
+++ ci/helpers/envLoader.groovy
@@ -2,14 +2,14 @@
 def load() {
   def envProps = new Properties()
   envProps.put("GIT_BRANCH", GIT_BRANCH)
-  
+
   echo envProps.toString()
   env.getEnvironment().each {
     envProps.put(it.key, it.value)
   }
   echo envProps.toString()
-  def globalProps = readProperties file: "ci/env/global-${env.envLanguage}.env", interpolate: true, defaults: envProps
-  def props = readProperties file: "ci/env/${env.envName}-${env.envLanguage}.env", interpolate: true, defaults: globalProps
+  def globalProps = readProperties file: "ci/env/global-${env.ENV_LANGUAGE}.env", interpolate: true, defaults: envProps
+  def props = readProperties file: "ci/env/${env.ENV_NAME.toLower()}-${env.ENV_LANGUAGE}.env", interpolate: true, defaults: globalProps
   keys = props.keySet()
   for (key in keys) {
     value = props["${key}"]
