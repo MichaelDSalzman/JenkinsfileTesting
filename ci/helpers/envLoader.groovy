ceae311024d5bd8e592479734e922cee0a91240e
Michael Salzman <michael.salzman@projekt202.com>
2021-10-22 20:53:05 -0500

    tweak

--- ci/helpers/envLoader.groovy
+++ ci/helpers/envLoader.groovy
@@ -1,5 +1,4 @@
 def load() {
-  echo ""
   def globalProps = readProperties file: "ci/env/global-${env.envLanguage}.env", interpolate: true
   def props = readProperties file: "ci/env/${env.envName}-${env.envLanguage}.env", interpolate: true, defaults: globalProps
   keys = props.keySet()
