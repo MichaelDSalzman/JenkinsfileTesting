fb9c782096da6704f98cf30f332253c86aa3619d
Michael Salzman <michael.salzman@projekt202.com>
2021-10-23 17:44:32 -0500

    Interpolation damnit

--- ci/helpers/envLoader.groovy
+++ ci/helpers/envLoader.groovy
@@ -1,6 +1,7 @@
 // TODO BETTER DOCS
 def load() {
   def envProps = new Properties().putAll(env.getEnvironment())
+  echo envProps.toString()
   def globalProps = readProperties file: "ci/env/global-${env.envLanguage}.env", interpolate: true, defaults: envProps
   def props = readProperties file: "ci/env/${env.envName}-${env.envLanguage}.env", interpolate: true, defaults: globalProps
   keys = props.keySet()
