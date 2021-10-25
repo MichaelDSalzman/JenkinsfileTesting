729b0178d048e4909da1e11069955a4950d84b3c
Michael Salzman <michael.salzman@projekt202.com>
2021-10-25 10:30:48 -0500

    Tweak sonar env var

--- ci/helpers/envLoader.groovy
+++ ci/helpers/envLoader.groovy
@@ -1,14 +1,16 @@
 // TODO BETTER DOCS
-def load() {
+def load(String envName, String envLanguage) {
   def envProps = new Properties()
   envProps.put("GIT_BRANCH", GIT_BRANCH)
+  envProps.put("ENV_NAME", envName)
+  envProps.put("ENV_LANGUAGE", envLanguage)
 
   env.getEnvironment().each {
     envProps.put(it.key, it.value)
   }
 
-  def globalProps = readProperties file: "ci/env/global-${env.ENV_LANGUAGE}.env", interpolate: true, defaults: envProps
-  def props = readProperties file: "ci/env/${env.ENV_NAME.toLowerCase()}-${env.ENV_LANGUAGE}.env", interpolate: true, defaults: globalProps
+  def globalProps = readProperties file: "ci/env/global-${envLanguage}.env", interpolate: true, defaults: envProps
+  def props = readProperties file: "ci/env/${envName.toLowerCase()}-${envLanguage}.env", interpolate: true, defaults: globalProps
   
   keys = props.keySet()
   for (key in keys) {
