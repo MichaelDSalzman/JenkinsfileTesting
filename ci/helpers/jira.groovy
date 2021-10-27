e2fff431426d35b608018f6c931fa8db617e78c8
Michael Salzman <michael.salzman@projekt202.com>
2021-10-27 14:09:32 -0500

    Pull reg ex into props

--- ci/helpers/jira.groovy
+++ ci/helpers/jira.groovy
@@ -1,5 +1,5 @@
 String findJiraIssueIdFromBranch() {
-def jiraIssueIdMatcher = env.GIT_BRANCH =~ /^.*?(AF-\d+)-.*$/
+def jiraIssueIdMatcher = env.GIT_BRANCH =~ env.jira_issue_id_git_branch_pattern
   if (jiraIssueIdMatcher.matches()) {
       return jiraIssueIdMatcher[0][1]
   }
