1eb902f82e2148ba141adffe30321384d6166c86
Michael Salzman <michael.salzman@projekt202.com>
2021-10-27 14:17:20 -0500

    Move comment into lib

--- ci/helpers/jira.groovy
+++ ci/helpers/jira.groovy
@@ -8,12 +8,10 @@ def jiraIssueIdMatcher = env.GIT_BRANCH =~ env.jira_issue_id_git_branch_pattern
 }
 
 void addComment(String jiraIssueId, String comment) {
-  script {
-      withEnv(['JIRA_SITE=EC2']) {
-          def input = [body: comment]
-          jiraAddComment idOrKey: jiraIssueId, input: input
-      }
-  }
+    withEnv(['JIRA_SITE=EC2']) {
+        def input = [body: comment]
+        jiraAddComment idOrKey: jiraIssueId, input: input
+    }
 }
 
 return this
