bfacf6a58cee9ad3a19d16ba1c10eaf6a5b6c693
Michael Salzman <michael.salzman@projekt202.com>
2022-01-03 13:24:23 -0600

    Load from env file

--- ci/jenkins/env.groovy
+++ ci/jenkins/env.groovy
@@ -1,1 +1,4 @@
-env.BUILD_STEPS_TO_INCLUDE=[env.BUILD_PIPELINE_STEPS_ENUM.INSTALL_DEPS]
+
+someEnums = load("ci/jenkins/someEnums.groovy")
+
+env.BUILD_STEPS_TO_INCLUDE=[someEnums.BuildPipelineSteps.INSTALL_DEPS]
